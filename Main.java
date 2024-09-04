6b23c987766711577406cf433f6339b22449b796
yassine chammakh <yassine.chammakh0@gmail.com>
2024-09-04 11:43:17 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,15 @@
 package kolo;
+import java.util.Scanner;
 public class Main{
     
 
 
 public static  void main (String[] args){
-    int value;
-    value=fibonachi(9);
+    long value; int index;
+    Scanner s = new Scanner(System.in);
+    System.out.printf("please what the index of fibunacci sequence that you want to know:\t");
+    index = s.nextInt();
+    value=fibonachi(index);
     System.out.println("the result is " +value);
 
 }
